74da73c15f28f102812ee4bdcc5d63fe42840a98
chenzRG <125750017+chenzRG@users.noreply.github.com>
2024-04-11 12:32:52 +0900

    Update Regularization.py

--- exp/Regularization.py
+++ exp/Regularization.py
@@ -1,12 +1,6 @@
 import torch
 class Regularization(torch.nn.Module):
     def __init__(self,model,weight_decay,p):
-        '''
-        :param model 模型
-        :param weight_decay:正则化参数
-        :param p: 范数计算中的幂指数值，默认求2范数,
-                  当p=0为L2正则化,p=1为L1正则化
-        '''
         super(Regularization, self).__init__()
         if weight_decay <= 0:
             print("param weight_decay can not <=0")
@@ -19,26 +13,16 @@ class Regularization(torch.nn.Module):
         #self.weight_info(self.weight_list)
  
     def to(self,device):
-        '''
-        指定运行模式
-        :param device: cude or cpu
-        :return:
-        '''
         self.device=device
         super().to(device)
         return self
  
     def forward(self, model):
-        self.weight_list=self.get_weight(model)#获得最新的权重
+        self.weight_list=self.get_weight(model)
         reg_loss = self.regularization_loss(self.weight_list, self.weight_decay, p=self.p)
         return reg_loss
  
     def get_weight(self,model):
-        '''
-        获得模型的权重列表
-        :param model:
-        :return:
-        '''
         weight_list = []
         for name, param in model.named_parameters():
             if 'weight' in name:
@@ -47,13 +31,6 @@ class Regularization(torch.nn.Module):
         return weight_list
  
     def regularization_loss(self,weight_list, weight_decay, p):
-        '''
-        计算张量范数
-        :param weight_list:
-        :param p: 范数计算中的幂指数值，默认求2范数
-        :param weight_decay:
-        :return:
-        '''
         # weight_decay=Variable(torch.FloatTensor([weight_decay]).to(self.device),requires_grad=True)
         # reg_loss=Variable(torch.FloatTensor([0.]).to(self.device),requires_grad=True)
         # weight_decay=torch.FloatTensor([weight_decay]).to(self.device)
@@ -67,11 +44,6 @@ class Regularization(torch.nn.Module):
         return reg_loss
  
     def weight_info(self,weight_list):
-        '''
-        打印权重列表信息
-        :param weight_list:
-        :return:
-        '''
         print("---------------regularization weight---------------")
         for name ,w in weight_list:
             print(name)
